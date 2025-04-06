8439f8df0ff366bcd2b193435906fad5e0197f3d
raffayuda <rafffa0206@gmail.com>
2025-04-06 17:27:43 +0700

    feat: Update dependencies and add new components for admin dashboard
    - Updated Next.js to version 14.2.5 and added new dependencies: react-big-calendar and react-calendar.
    - Enhanced global styles in globals.css for better scrollbar handling and calendar styling.
    - Created a new AdminPage component with user cards, attendance chart, count chart, finance chart, and event calendar.
    - Implemented a DashboardLayout component for consistent layout across dashboard pages.
    - Added ParentPage, StudentPage, TeacherPage, and LoginPage components with basic structure.
    - Developed reusable components: Announcements, AttendanceChart, BigCalendar, CountChart, EventCalendar, FinanceChart, Navbar, and UserCard.
    - Updated tailwind.config.ts to include custom colors for better theming.
    - Modified exam dates in data.ts for upcoming events.

---
+++ src/components/Announcements.tsx
@@ -0,0 +1,36 @@
+const Announcements = () => {
+  return (
+    <div className='bg-white rounded-md p-4'>
+      <div className="flex items-center justify-between">
+        <h1 className="text-xl font-semibold">Announcements</h1>
+        <span className="text-xs text-gray-400">View All</span>
+      </div>
+      <div className="flex flex-col gap-4 mt-4"></div>
+      <div className="bg-lamaSkyLight rounded-md p-4">
+        <div className="flex items-center justify-between">
+          <h2 className="font-medium">Lorem ipsum dolor.</h2>
+          <span className="text-xs text-gray-400 bg-white rounded-md px-1 py-1">2025-01-01</span>
+        </div>
+        <p className="text-sm text-gray-400">Lorem ipsum dolor sit amet consectetur adipisicing elit. Et mollitia similique sequi aliquid officiis voluptatem ut unde. Dolorum aperiam quibusdam natus vitae suscipit repellat sunt? Iusto dicta quam repudiandae corrupti!</p>
+      </div>
+      <div className="flex flex-col gap-4 mt-4"></div>
+      <div className="bg-lamaPurpleLight rounded-md p-4">
+        <div className="flex items-center justify-between">
+          <h2 className="font-medium">Lorem ipsum dolor.</h2>
+          <span className="text-xs text-gray-400 bg-white rounded-md px-1 py-1">2025-01-01</span>
+        </div>
+        <p className="text-sm text-gray-400">Lorem ipsum dolor sit amet consectetur adipisicing elit. Et mollitia similique sequi aliquid officiis voluptatem ut unde. Dolorum aperiam quibusdam natus vitae suscipit repellat sunt? Iusto dicta quam repudiandae corrupti!</p>
+      </div>
+      <div className="flex flex-col gap-4 mt-4"></div>
+      <div className="bg-lamaYellowLight rounded-md p-4">
+        <div className="flex items-center justify-between">
+          <h2 className="font-medium">Lorem ipsum dolor.</h2>
+          <span className="text-xs text-gray-400 bg-white rounded-md px-1 py-1">2025-01-01</span>
+        </div>
+        <p className="text-sm text-gray-400">Lorem ipsum dolor sit amet consectetur adipisicing elit. Et mollitia similique sequi aliquid officiis voluptatem ut unde. Dolorum aperiam quibusdam natus vitae suscipit repellat sunt? Iusto dicta quam repudiandae corrupti!</p>
+      </div>
+    </div>
+  )
+}
+
+export default Announcements
