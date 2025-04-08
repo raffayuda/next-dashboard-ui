2a275491de7553768a5cf91421402cbbd5e0e8d1
raffayuda <rafffa0206@gmail.com>
2025-04-08 12:46:43 +0700

    feat: Update links in Student and Teacher pages for improved navigation and query handling

--- src/app/(dashboard)/list/teachers/page.tsx
+++ src/app/(dashboard)/list/teachers/page.tsx
@@ -5,7 +5,7 @@ import TableSearch from "@/components/TableSearch";
 import { role, teachersData } from "@/lib/data";
 import prisma from "@/lib/prisma";
 import { ITEM_PER_PAGE } from "@/lib/settings";
-import { Class, Subject, Teacher } from "@prisma/client";
+import { Class, Prisma, Subject, Teacher } from "@prisma/client";
 import Image from "next/image";
 import Link from "next/link";
 
@@ -93,8 +93,34 @@ const TeacherListPage = async ({ searchParams }: { searchParams: { [key: string]
 
   const p = page ? parseInt(page) : 1;
 
+  // URL PARAMS CONDITION
+
+  const query: Prisma.TeacherWhereInput = {};
+
+  if (queryParams) {
+    for (const [key, value] of Object.entries(queryParams)) {
+      if (value !== undefined) {
+        switch (key) {
+          case "classId":
+            query.lessons = {
+              some: {
+                classId: parseInt(value),
+              },
+            };
+            break;
+          case "search":
+            query.name = { contains: value, mode: "insensitive" };
+            break;
+          default:
+            break;
+        }
+      }
+    }
+  }
+
   const [data, count] = await prisma.$transaction([
     prisma.teacher.findMany({
+      where: queryParams,
       include: {
         subjects: true,
         classes: true,
@@ -102,7 +128,11 @@ const TeacherListPage = async ({ searchParams }: { searchParams: { [key: string]
       take: ITEM_PER_PAGE,
       skip: (Number(p) - 1) * ITEM_PER_PAGE
     }),
-    prisma.teacher.count()
+    prisma.teacher.count({where: {
+      lessons:{
+        some: {classId:parseInt(queryParams.classId!)},
+      }
+    }})
   ])
 
 
